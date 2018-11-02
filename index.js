fcca9ee2b373d186a6062af52fa9c6335444e993
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-02 14:52:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver {
+  constructor (name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf (endDate) {
+    this.endDate = new Date(endDate)
+    return this.endDate.getFullYear() - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled () {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ]
+    let verticalDistance = parseInt(
+      this.endingLocation.vertical - this.beginningLocation.vertical
+    )
+    let horizontalDistance =
+      eastWest.indexOf(this.endingLocation.horizontal) -
+      eastWest.indexOf(this.beginningLocation.horizontal)
+    return verticalDistance + horizontalDistance
+  }
+
+  estimatedTime (isPeakHour) {
+    let distance = this.blocksTravelled()
+    if (isPeakHour === true) {
+      return distance / 2
+    } else {
+      return distance / 3
+    }
+  }
+}
